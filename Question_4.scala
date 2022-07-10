75d00f9a0804aa500289a56f30b3d149a2cfdf60
janith2021 <92769989+janith2021@users.noreply.github.com>
2022-07-10 20:42:54 +0530

    Add files via upload

---
+++ Question_4.scala
@@ -0,0 +1,28 @@
+object Question_4 {
+  def main(args:Array[String]): Unit ={
+    printf("Your TakeHome Salary= Rs: %.3f\n",TakeHomeSalary(40,30))
+    printf("Your Profit= %d\n",totalProfit(20))
+    printf("Your Profit= %d\n",totalProfit(15))
+    printf("Your Profit= %d\n",totalProfit(10))
+  }
+
+  // 4 a)
+  def wage(h1:Int)=250*h1
+  def OT(h2:Int)=85*h2
+  def Income(h1:Int,h2:Int)=OT(h2)+wage(h1)
+  def Tax(income:Double)=0.12*income
+
+  def TakeHomeSalary(h1:Int,h2:Int): Double ={
+    Income(h1,h2)-Tax(Income(h1:Int,h2:Int))
+  }
+
+  // 4 b)
+
+  def atendees(price:Int)=120+(15-price)/5*20
+  def income(price:Int)=atendees(price)*price
+  def totalCost(price:Int)=atendees(price)*3+500
+
+  def totalProfit(price:Int):Int={
+    income(price)-totalCost(price)
+  }
+}
